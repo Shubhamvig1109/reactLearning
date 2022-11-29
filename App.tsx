3303d7c657e4079125260f9a45abb9c7ca6dbffc
Shubhamvig1109 <115772479+Shubhamvig1109@users.noreply.github.com>
2022-11-29 22:21:53 +0530

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,64 @@
 import * as React from 'react';
 import './style.css';
-
+import './Form.tsx';
 export default function App() {
+  const [state, setState] = React.useState([]);
+
+  const [saveData, setsaveData] = React.useState([]);
+
+  const Savedatamain = (key, index, value) => {
+    for (var i = 0; i < state.length; i++) {
+      let newObj = state[i];
+      if (index === i) {
+        newObj[key] = value;
+      }
+    }
+  };
+  const CreateForm = () => {
+    let objectContainer = {
+      Name: '',
+      surname: '',
+      location: '',
+    };
+
+    const secondArray = [...state];
+    secondArray.push(objectContainer);
+    setState(secondArray);
+    console.log(state);
+  };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={CreateForm}>Click</button>
+      <button>Save</button>
+      {state?.map((item, index) => {
+        return (
+          <div>
+            <div>
+              <p>NAME</p>
+              <input
+                type="text"
+                onChange={(e) => Savedatamain('Name', index, e.target.value)}
+              />
+            </div>
+            <div>
+              <p>SURNAME</p>
+              <input
+                type="text"
+                onChange={(e) => Savedatamain('surname', index, e.target.value)}
+              />
+            </div>
+            <div>
+              <p>LOCATION</p>
+              <input
+                type="text"
+                onChange={(e) =>
+                  Savedatamain('location', index, e.target.value)
+                }
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
